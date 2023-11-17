9d37ed12592e6153957e4bc4ee52819de48c3c7d
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-11-17 13:20:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import replicate
 import os
+import langchain  # Import Langchain
 
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
@@ -13,12 +14,17 @@ with st.sidebar:
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+        if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+# Function to read PDF data using Langchain
+def read_pdf(file_path):
+    with langchain.document(file_path) as doc:
+        return doc.get_text()
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -34,17 +40,18 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
-# Refactored from https://github.com/a16z-infra/llama2-chatbot
-def generate_llama2_response(prompt_input):
+def generate_llama2_response(prompt_input, pdf_data=""):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
+    # Include PDF data in the prompt
+    prompt = f"{string_dialogue} {prompt_input} Assistant: {pdf_data}"
+
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
+                           input={"prompt": prompt, "temperature": 0.1, "top_p": 0.9, "max_length": 512, "repetition_penalty": 1})
     return output
 
 # User-provided prompt
@@ -53,11 +60,14 @@ if prompt := st.chat_input(disabled=not replicate_api):
     with st.chat_message("user"):
         st.write(prompt)
 
-# Generate a new response if last message is not from assistant
+# Generate a new response if the last message is not from the assistant
 if st.session_state.messages[-1]["role"] != "assistant":
+    # Retrieve PDF data (replace 'your_pdf_path.pdf' with the actual path)
+    pdf_data = read_pdf('merge.pdf')
+
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt)
+            response = generate_llama2_response(prompt, pdf_data)
             placeholder = st.empty()
             full_response = ''
             for item in response:
