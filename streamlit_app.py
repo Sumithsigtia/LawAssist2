009d474d22f6e57a6fe936a1e3af820efb5597d7
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-11-18 13:03:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import replicate
 import os
-import langchain
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="🦙💬 Legal Assist Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
+    st.title('🦙💬 Legal Assist Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
@@ -21,19 +20,9 @@ with st.sidebar:
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-# Function to read PDF data using Langchain
-def read_pdf(file_path):
-    pdf_data = ""
-    try:
-        with langchain.document(file_path) as doc:
-            pdf_data = doc.get_text()
-    except Exception as e:
-        st.error(f"Error reading PDF: {e}")
-    return pdf_data
-
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with legal matters today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
@@ -41,45 +30,35 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with legal matters today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
-def generate_llama2_response(prompt_input, pdf_data=""):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+# Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a legal assistant. You specialize in providing assistance with legal matters. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-
-    # Include user's question and relevant information from the PDF in the prompt
-    prompt = f"{string_dialogue} User: {prompt_input}\n\nAssistant: {pdf_data}"
-
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
-                           input={"prompt": prompt, "temperature": 0.1, "top_p": 0.9, "max_length": 512, "repetition_penalty": 1})
+    internal_prompt = "Assistant: I am acting as a legal assistant. Please provide details about your legal query or concern."
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {internal_prompt} {prompt_input} Assistant: ",
+                                  "temperature": 0.1, "top_p": 0.9, "max_length": 512, "repetition_penalty": 1})
     return output
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
+if prompt := st.chat_input(disabled=not replicate_api, placeholder="Please describe your legal query or concern..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
 # Generate a new response if the last message is not from the assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    # Retrieve a limited amount of PDF data (replace 'your_pdf_path.pdf' with the actual path)
-    pdf_data = read_pdf('merge.pdf')
-
-    # Limit the length of the input prompt and PDF data
-    max_prompt_length = 256
-    max_pdf_data_length = 1024
-    prompt_input = prompt[:max_prompt_length]
-    pdf_data = pdf_data[:max_pdf_data_length]
-
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt_input, pdf_data)
+            response = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
             for item in response:
@@ -88,3 +67,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
+
