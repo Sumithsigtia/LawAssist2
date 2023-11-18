bd1484a47f0fa8ff1a7dd42f112d4990c2d67da9
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-11-18 19:25:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,26 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Legal Assist Chatbot")
+st.set_page_config(page_title="Legal Assist Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Legal Assist Chatbot')
+    st.title('Legal Assist Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
+        st.success('API key already provided!')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+            st.warning('Please enter your credentials!')
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+            st.success('Proceed to entering your prompt message!')
+    st.markdown('Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with legal matters today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with Indian legal matters today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
@@ -30,26 +30,26 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with legal matters today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with Indian legal matters today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a legal assistant. You specialize in providing assistance with legal matters. You only respond once as 'Assistant'."
+    string_dialogue = "You are a legal assistant specializing in Indian law. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
-            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+            string_dialogue += f"User: {dict_message['content']}\n\n"
         else:
-            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    internal_prompt = "Assistant: I am acting as a legal assistant. Please provide details about your legal query or concern."
+            string_dialogue += f"Assistant: {dict_message['content']}\n\n"
+    internal_prompt = "Assistant: I am acting as a legal assistant specializing in Indian law. Please provide details about your legal query or concern."
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {internal_prompt} {prompt_input} Assistant: ",
                                   "temperature": 0.1, "top_p": 0.9, "max_length": 512, "repetition_penalty": 1})
     return output
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api, placeholder="Please describe your legal query or concern..."):
+if prompt := st.chat_input(disabled=not replicate_api, placeholder="Please describe your Indian legal query or concern..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
@@ -67,4 +67,3 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
-
