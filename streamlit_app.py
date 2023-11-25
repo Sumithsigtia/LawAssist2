de8cf0717cdf66a93a03268faba1a5fc68bf8063
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-11-25 11:15:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,28 +3,30 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="Legal Assist Chatbot")
-st.title('Ask me legal questions, and I\'ll provide answers!\n'
+st.set_page_config(page_title="Legal Assistant Chatbot")
+st.title('Law Assist\n'
          'Created by: Vidhan Mehta, Sumith Sigtia, Shabiul Hasnain Siddiqui, Swathi')
-
 # Replicate Credentials
 with st.sidebar:
-    st.title('Legal Assist Chatbot')
+    st.title('Legal Assistant Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!')
+        st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-            st.warning('Please enter your credentials!')
+            st.warning('Please enter your credentials!', icon='⚠️')
         else:
-            st.success('Proceed to entering your prompt message!')
-
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+# Use Llama2-70B model
+llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with Indian legal matters today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "You are a legal assistant chatbot specializing in Indian law. How may I assist you today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
@@ -32,45 +34,29 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you with Indian legal matters today?"}]
-
+    st.session_state.messages = [{"role": "assistant", "content": "You are a legal assistant chatbot specializing in Indian law. How may I assist you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
-# Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
-    try:
-        string_dialogue = "You are a legal assistant specializing in Indian law. You only respond once as 'Assistant'."
-        for dict_message in st.session_state.messages:
-            if dict_message["role"] == "user":
-                string_dialogue += f"User: {dict_message['content']}\n\n"
-            else:
-                string_dialogue += f"Assistant: {dict_message['content']}\n\n"
-        internal_prompt = "Assistant: I am acting as a legal assistant specializing in Indian law. Please provide details about your legal query or concern."
-        input_prompt = f"{string_dialogue} {internal_prompt} {prompt_input} Assistant: "
-        st.write(f"Input prompt: {input_prompt}")
-
-        # Iterate through the generator to get the actual text
-        output_generator = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
-                                         input={"prompt": input_prompt,
-                                                "temperature": 0.1, "top_p": 0.9, "max_length": 512, "repetition_penalty": 1})
-        output_list = list(output_generator)
-        full_response = ''.join(output_list)
-        st.write(f"Output from Replicate: {full_response}")
-
-        return output_list
-
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        return []
+    string_dialogue = "You are a helpful legal assistant chatbot specializing in Indian law. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run(llm, 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature": 0.1, "top_p": 0.9, "max_length": 512, "repetition_penalty": 1})
+    return output
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api, placeholder="Please describe your Indian legal query or concern..."):
+if prompt := st.chat_input(disabled=not replicate_api):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
-# Generate a new response if the last message is not from the assistant
+# Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
